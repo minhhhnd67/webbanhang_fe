f234270dd845659fa046a8d22648532e83686aa0
minhnd31 <v.minhnd31@vinschool.edu.vn>
2023-12-10 01:21:44 +0700

    feature/minhnd/client-register

--- src/components/customer/Login.vue
+++ src/components/customer/Login.vue
@@ -1,48 +1,67 @@
 <template>
   <el-container>
     <el-main>
-      <el-row type="flex" justify="center" style="height: 450px;">
-        <el-col :span="12" style="background-color: #ffd400; border-radius: 30px;">
+      <el-row type="flex" justify="center" style="height: 450px">
+        <el-col :span="12" style="background-color: #ffd400; border-radius: 30px">
           <el-row type="flex" justify="center">
             <el-col :span="6">
-              <p style="font-size: 32px; tex-align: center;"><b>Đăng nhập</b></p>
+              <p style="font-size: 32px; tex-align: center"><b>Đăng nhập</b></p>
             </el-col>
           </el-row>
-          <el-row style="margin-top: 10px;" type="flex" justify="center">
+          <el-row style="margin-top: 10px" type="flex" justify="center">
             <el-col :span="16">
               <el-input placeholder="Email" v-model="email"></el-input>
             </el-col>
           </el-row>
-          <el-row style="margin-top: 20px;" type="flex" justify="center">
+          <el-row style="margin-top: 20px" type="flex" justify="center">
             <el-col :span="16">
               <el-input placeholder="Password" v-model="password" show-password>
               </el-input>
             </el-col>
           </el-row>
-          <el-row style="margin-top: 20px;" type="flex" justify="center">
+          <el-row style="margin-top: 20px" type="flex" justify="center">
             <el-col :span="16">
-              <el-button  @click="handleLogin()"  style="width: 100%; background-color: #ff5100; color: #ffffff;">Đăng nhập</el-button>
+              <el-button
+                @click="handleLogin()"
+                style="width: 100%; background-color: #ff5100; color: #ffffff"
+                >Đăng nhập</el-button
+              >
             </el-col>
           </el-row>
-          <el-row style="margin-top: 20px;" type="flex" justify="center">
+          <el-row style="margin-top: 20px" type="flex" justify="center">
             <el-col :span="4">
               <el-row type="flex" justify="center">
-                <el-button @click="loginWithGoogle()" style="background-color: #ffd400; border: 0px;">
-                  <img style="width: 50px;" src="@/icons/google.svg" alt="Login using Google"/>
+                <el-button
+                  @click="loginWithGoogle()"
+                  style="background-color: #ffd400; border: 0px"
+                >
+                  <img
+                    style="width: 50px"
+                    src="@/icons/google.svg"
+                    alt="Login using Google"
+                  />
                 </el-button>
               </el-row>
             </el-col>
             <el-col :span="4">
               <el-row type="flex" justify="center">
-                <el-button style="background-color: #ffd400; border: 0px;">
-                  <img style="width: 50px;" src="@/icons/facebook.svg" alt="Login using Facebook"/>
+                <el-button style="background-color: #ffd400; border: 0px">
+                  <img
+                    style="width: 50px"
+                    src="@/icons/facebook.svg"
+                    alt="Login using Facebook"
+                  />
                 </el-button>
               </el-row>
             </el-col>
           </el-row>
-          <el-row style="margin-top: 20px;" type="flex" justify="center">
+          <el-row style="margin-top: 20px" type="flex" justify="center">
             <el-col :span="8">
-              <p>Bạn chưa có tài khoản?<b style="color: #ff5100;"><a @click="register()">Đăng ký</a></b></p>
+              <p>
+                Bạn chưa có tài khoản?<b style="color: #ff5100"
+                  ><a @click="register()">Đăng ký</a></b
+                >
+              </p>
             </el-col>
           </el-row>
         </el-col>
@@ -51,10 +70,10 @@
   </el-container>
 </template>
 <script>
-import EventBus from '@/utils/EventBus.js';
+import EventBus from "@/utils/EventBus.js";
 import router from "@/router";
 import store from "@/store";
-import { loginGoogle, login } from '@/api/manager/auth.js';
+import { loginGoogle, login } from "@/api/manager/auth.js";
 
 export default {
   name: "C-Login",
@@ -66,7 +85,7 @@ export default {
       token: "",
       login_google: false,
       login_facebook: false,
-    }
+    };
   },
   mounted() {
     // Tạo một kênh
@@ -77,12 +96,12 @@ export default {
       const data = event.data;
       this.login_google = data.login_google;
       this.token = data.token;
-      localStorage.setItem('tokenBE', this.token);
+      localStorage.setItem("tokenBE", this.token);
       store.state.permission = 1;
       store.state.tokenBE = this.token;
       store.state.is_login_manager = false;
       // Xử lý dữ liệu
-      EventBus.$emit('emit-auth', {
+      EventBus.$emit("emit-auth", {
         isLogin: true,
       });
       router.push({ name: "c-home" }).catch(() => {});
@@ -92,35 +111,36 @@ export default {
   methods: {
     emitEvent(isSearch = false) {
       console.log(222);
-      EventBus.$emit('search-product', {
+      EventBus.$emit("search-product", {
         storeId: this.storeId,
         search: this.search,
         isSearch: isSearch,
       });
     },
     async loginWithGoogle() {
-      const response = await loginGoogle()
-      window.open(
-        response.data.url,
-      );
+      const response = await loginGoogle();
+      window.open(response.data.url);
     },
     async handleLogin() {
       const data = {
         email: this.email,
-        password: this.password
-      }
-      const response = await login(data)
+        password: this.password,
+      };
+      const response = await login(data);
       if (response.data.code == 200) {
-        store.state.permission = 1
-        store.state.tokenBE = response.data.data.token
-        localStorage.setItem('tokenBE', response.data.data.token)
-        store.state.is_login_manager = false
-        router.push({ name: "m-home" })
+        store.state.permission = 1;
+        store.state.tokenBE = response.data.data.token;
+        localStorage.setItem("tokenBE", response.data.data.token);
+        store.state.is_login_manager = false;
+        EventBus.$emit("emit-auth", {
+          isLogin: true,
+        });
+        router.push({ name: "c-home" });
       }
     },
     register() {
       router.push({ name: "c-register" });
     },
-  }
+  },
 };
 </script>
